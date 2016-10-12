098ae4830e29d7456ae25d2bcb25619488ac0b44
Omar Montano Rivas <omarmrivas@gmail.com>
2016-10-12 14:53:29 -0500

    Corregidos los nombres de las tablas en los procedimiento

--- datos_entrenamiento.sql
+++ datos_entrenamiento.sql
@@ -6,8 +6,8 @@ BEGIN
            (A.materia IS NOT NULL) AND
            (A.final IS NOT NULL) AND
            NOT (A.final IN('RV.','RV','REV','RE','Q','EQ.','EQ','E')) AND
-           (EXISTS (SELECT * FROM Grupos G WHERE (G.grupo = A.grupo))) AND
-           (EXISTS (SELECT * FROM Planes P WHERE (A.materia = P.clave))) AND
+           (EXISTS (SELECT * FROM grupos G WHERE (G.grupo = A.grupo))) AND
+           (EXISTS (SELECT * FROM planes P WHERE (A.materia = P.clave))) AND
             EXISTS (SELECT * FROM kardex B
     	  	  WHERE (B.materia = codigo AND A.matricula = B.matricula AND
 		            (A.semestre < B.semestre OR A.materia = codigo) AND
@@ -16,8 +16,8 @@ BEGIN
                     NOT (B.final IN('RV.','RV','REV','RE','Q','EQ.','EQ','E')) AND
                     (B.periodo >= periodoInicial AND B.periodo <= periodoFinal) AND
                     (B.materia IS NOT NULL) AND
-                    (EXISTS (SELECT * FROM Grupos G WHERE (G.grupo = B.grupo))) AND
-                    (EXISTS (SELECT * FROM Planes P 
+                    (EXISTS (SELECT * FROM grupos G WHERE (G.grupo = B.grupo))) AND
+                    (EXISTS (SELECT * FROM planes P 
                         WHERE (B.materia = P.clave))))))
 ORDER BY matricula, materia, periodo;
 END //
--- datos_prediccion.sql
+++ datos_prediccion.sql
@@ -6,8 +6,8 @@ BEGIN
            (A.materia IS NOT NULL) AND
            (A.final IS NOT NULL) AND
            NOT (A.final IN('RV.','RV','REV','RE','Q','EQ.','EQ','E')) AND
-           (EXISTS (SELECT * FROM Grupos G WHERE (G.grupo = A.grupo))) AND
-           (EXISTS (SELECT * FROM Planes P WHERE (A.materia = P.clave))) AND
+           (EXISTS (SELECT * FROM grupos G WHERE (G.grupo = A.grupo))) AND
+           (EXISTS (SELECT * FROM planes P WHERE (A.materia = P.clave))) AND
            (EXISTS (SELECT * FROM Kardex B WHERE (A.matricula = B.matricula AND B.materia = codigo AND
                                                   B.periodo = periodoPrediccion)))) OR
            (A.materia = codigo AND A.periodo = periodoPrediccion AND
